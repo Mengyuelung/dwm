87d01fd958dd41476cc073f8747f4a79b2f7d1b5
WuLong <rszen@sina.cn>
2023-03-21 16:19:24 +0800

    change fonts and status

--- scripts/bar.sh
+++ scripts/bar.sh
@@ -1,34 +1,53 @@
-#!/usr/local/bin/bash
+#!/usr/bin/bash
 
 # ^c$var^ = fg color
 # ^b$var^ = bg color
 
+interval=0
+
 # load colors
-. $HOME/.dwm/scripts/bar_themes/onedark
+. ~/.dwm/scripts/bar_themes/onedark
 
-version() {
-  ver=$(freebsd-version)
-  printf "^c$blue^^b$black^ "
-  printf "^c$blue^ $ver"
+cpu() {
+  cpu_val=$(grep -o "^[^ ]*" /proc/loadavg)
+
+  printf "^c$black^ ^b$green^ CPU"
+  printf "^c$white^ ^b$grey^ $cpu_val"
 }
 
-ram() {
-  ram_use=$(freecolor -o|sed -n '2p'|awk '{print $3}')
-  ram_total=$(freecolor -o|sed -n '2p'|awk '{print $2}')
-  ram_pec=$(awk 'BEGIN{printf "%.f", ('$ram_use'/'$ram_total')*100 }')
-  printf "^c$blue^^b$black^ "
-  printf "^c$blue^ $ram_pec%%"
+pkg_updates() {
+  #updates=$({ timeout 20 doas xbps-install -un 2>/dev/null || true; } | wc -l) # void
+  updates=$({ timeout 20 checkupdates 2>/dev/null || true; } | wc -l) # arch
+  # updates=$({ timeout 20 aptitude search '~U' 2>/dev/null || true; } | wc -l)  # apt (ubuntu, debian etc)
+
+  if [ -z "$updates" ]; then
+    printf "  ^c$green^    Fully Updated"
+  else
+    printf "  ^c$green^    $updates"" updates"
+  fi
 }
 
-battery() {
-  printf "^c$blue^  test"
+mem() {
+  printf "^c$blue^^b$black^  "
+  printf "^c$blue^ $(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)"
+}
+
+wlan() {
+	case "$(cat /sys/class/net/wl*/operstate 2>/dev/null)" in
+	up) printf "^c$black^ ^b$blue^ 󰤨 ^d^%s" " ^c$blue^Connected" ;;
+	down) printf "^c$black^ ^b$blue^ 󰤭 ^d^%s" " ^c$blue^Disconnected" ;;
+	esac
 }
 
 clock() {
+	printf "^c$black^ ^b$darkblue^ 󱑆 "
 	printf "^c$black^^b$blue^ $(date '+%H:%M')  "
 }
 
-
 while true; do
-  sleep 1 && xsetroot -name "$(version) $(ram) $(battery) $(clock)"
+
+  [ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates)
+  interval=$((interval + 1))
+
+  sleep 1 && xsetroot -name "$updates $(cpu) $(mem) $(wlan) $(clock)"
 done
